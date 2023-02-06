05631ef878f1f56925f8a5bda98c2c617838d3ad
Raul Pereira <raul.pereira@gmail.com>
2023-02-05 22:20:26 -0300

    implement to_string

--- dungeon_crawl/lib/dungeon_crawl/room/action.ex
+++ dungeon_crawl/lib/dungeon_crawl/room/action.ex
@@ -8,4 +8,8 @@ defmodule DungeonCrawl.Room.Action do
   def rest, do: %Action{id: :rest, label: "Take a better look and rest."}
 
   def search, do: %Action{id: :search, label: "Search the room."}
+
+  defimpl String.Chars do
+    def to_string(action), do: action.label
+  end
 end

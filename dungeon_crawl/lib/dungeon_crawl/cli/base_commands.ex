1ed27d868ba20cc52bee7fcd7d899bcaabe1d268
Raul Pereira <raul.pereira@gmail.com>
2023-02-05 22:16:10 -0300

    refactoring with defprotocol and defimpl

--- dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
+++ dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
@@ -5,7 +5,7 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     options
     |> Enum.with_index(1)
     |> Enum.each(fn {option, index} ->
-      Shell.info("#{index} - #{option}")
+      Shell.info("#{index} - #{DungeonCrawl.Display.info(option)}")
     end)
 
     options

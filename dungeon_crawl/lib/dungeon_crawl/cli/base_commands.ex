fc1b9a64e365a2eb94565c51b7634db9475f5d5f
Raul Pereira <raul.pereira@gmail.com>
2023-02-12 22:19:32 -0300

    add throw and catch flow

--- dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
+++ dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
@@ -1,6 +1,8 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  @invalid_option {:error, "Invalid option"}
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
@@ -17,18 +19,18 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     "Which one? [#{options}]\n"
   end
 
-  def parse_answer!(answer) do
+  def parse_answer(answer) do
     case Integer.parse(answer) do
       :error ->
-        raise DungeonCrawl.CLI.InvalidOptionError
+        throw(@invalid_option)
 
       {option, _} ->
         option - 1
     end
   end
 
-  def find_option_by_index!(index, options) do
-    Enum.at(options, index) || raise DungeonCrawl.CLI.InvalidOptionError
+  def find_option_by_index(index, options) do
+    Enum.at(options, index) || throw(@invalid_option)
   end
 
   def ask_for_option(options) do
@@ -37,18 +39,18 @@ defmodule DungeonCrawl.CLI.BaseCommands do
       |> display_options
       |> generate_question
       |> Shell.prompt()
-      |> parse_answer!
-      |> find_option_by_index!(options)
-    rescue
-      e in DungeonCrawl.CLI.InvalidOptionError ->
-        display_error(e)
+      |> parse_answer
+      |> find_option_by_index(options)
+    catch
+      {:error, message} ->
+        display_error(message)
         ask_for_option(options)
     end
   end
 
-  def display_error(e) do
+  def display_error(message) do
     Shell.cmd("clear")
-    Shell.error(e.message)
+    Shell.error(message)
     Shell.prompt("Press Enter to continue.")
     Shell.cmd("clear")
   end
